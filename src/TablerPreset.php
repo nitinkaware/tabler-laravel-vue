e279636baba327fe6901b0ec317d24f267f41633
Nitin Kaware <nitin.kaware1@gmail.com>
2019-06-24 16:38:48 +0530

    add stub

--- src/TablerPreset.php
+++ src/TablerPreset.php
@@ -3,6 +3,7 @@
 namespace NitinKaware\TablerPreset;
 
 use cesaramirez\Presets\Tabler\TablerPreset as BaseTablerPreset;
+use Illuminate\Container\Container;
 use Illuminate\Filesystem\Filesystem;
 
 class TablerPreset extends BaseTablerPreset {
@@ -48,6 +49,34 @@ class TablerPreset extends BaseTablerPreset {
         copy(__DIR__ . '/tabler-stub/webpack.mix.js', base_path('webpack.mix.js'));
 
         (new Filesystem())->copyDirectory(__DIR__ . '/tabler-stub/Pages', resource_path('js/Pages'));
+    }
+
+    /**
+     * Export the authentication views.
+     */
+    protected static function scaffoldAuth()
+    {
+        file_put_contents(app_path('Http/Controllers/HomeController.php'), static::compileControllerStub());
+        file_put_contents(
+            base_path('routes/web.php'),
+            "\nAuth::routes();\n\nRoute::get('/home', 'HomeController@index')->name('home');\n\n",
+            FILE_APPEND
+        );
+        (new Filesystem())->copyDirectory(__DIR__.'/tabler-stubs/views', resource_path('views'));
+    }
+
+    /**
+     * Compiles the HomeController stub.
+     *
+     * @return string
+     */
+    protected static function compileControllerStub()
+    {
+        return str_replace(
+            '{{namespace}}',
+            Container::getInstance()->getNamespace(),
+            file_get_contents(__DIR__.'/tabler-stubs/controllers/HomeController.stub')
+        );
     }
 
     /**
